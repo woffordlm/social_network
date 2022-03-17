15ee6b2f420c0db42a3c0f0e2b6332e66a0a0001
woffordlm <lukewofford@gmail.com>
2022-03-17 10:33:43 -0400

    initial setup

---
+++ models/index.js
@@ -0,0 +1,7 @@
+
+const Users = require("./Users");
+const Thoughts = require("./Thoughts");
+const Reactions = require("./Reactions");
+const Friends = require("./Friends");
+
+module.export= {Users, Thoughts, Reactions, Friends}
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = reguire("mongoose");
+const format = require('date-fns/format');
+// const stPattysDay = new Date('2020/03/17');
+// const formattedDate1 = format(stPattysDay, 'MM/dd/yyyy');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+ app.use(express.json());
+ app.use(express.urlencoded({extended:true}));
+ app.use(express.static("public"));
+
+ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/nosql_social_network_api' {
+    useNewUrlParser: true,
+    useUnifiedTopology: true    
+ });
+
+
+ // Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
