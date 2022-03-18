8b70c28afee33c4b60f3dbea6a7f4ae2d062b2f3
woffordlm <lukewofford@gmail.com>
2022-03-18 09:56:09 -0400

    created api routes

--- models/User.js
+++ models/User.js
@@ -1,12 +1,39 @@
-const { Schema, model, Types } = require('mongoose');
+const {
+    Schema,
+    model,
+    Types
+} = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const UserSchema = new Schema (
-    {
-        username: {
-            type: String,
-            required:true,
-            
-        }
-    }
-)
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
+})
+
+// Virtuals allow you to add virtual properties to a document that aren't 
+// stored in the database. They're normally computed values that get evaluated 
+// when you try to access their properties.
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length
+})
+const User = model('User', UserSchema);
+module.exports = User
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,24 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers, 
+    getUser,
+    createNewUser,
+    updateUser,
+    deleteUser
+} = require("../controller/user-controller")
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .get(getUser)
+    .post(createNewUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+
+// make route for get all users
+// get a single user by its _id and populated thought and friend data
+// Post a new user
+// put to update a suer byits _id
+// Delete to remove user by it _id
